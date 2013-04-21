4356c2951ee81a4fe0546880d8742fa82a830a51
Artoria <Artoria>
2013-04-22 06:02:15 +0800

    SRC

--- header.h
+++
@@ -1,8 +0,0 @@
-
-  DATA(strings)
-    STRING(9, "echo Extraction Finished.");
-    STRING(9, "echo Will run main.rb");
-    STRING(9, "type main.rb");
-    STRING(9, "echo.");
-    STRING(9, "ruby --disable-gems -I. main.rb");
-  END(strings)
--- pack.h
+++
@@ -1,8 +0,0 @@
-
-
-
-DIR(main)                                                    
-  FILE(C:/ruby200/bin/ruby.exe)                             
-  FILE(C:/ruby200/bin/msvcrt-ruby200.dll)                   
-  FILE(main.rb)                                             
-  FILE(ubygems.rb)                                         
