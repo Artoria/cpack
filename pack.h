bb66734b02f9802b4cbb18d5390df80e7a62cdfb
Artoria <pochy2008@qq.com>
2013-04-22 03:18:04 +0800

    Create pack.h

---
+++ pack.h
@@ -0,0 +1,5 @@
+DIR(main)                                                    
+  FILE(C:/ruby200/bin/ruby.exe)                             
+  FILE(C:/ruby200/bin/msvcrt-ruby200.dll)                   
+  FILE(main.rb)                                             
+  FILE(ubygems.rb)                                         
